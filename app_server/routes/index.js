a0841ab6ed8f8ea6df90bc61e98e87a4d5dd1873
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-25 02:15:08 +0300

    #6@1h01m: create customisable routes and controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+// Locations pages
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlOthers.about);
+
+// Other pages
+router.get('/about', ctrlLocations.homelist);
 
 module.exports = router;
