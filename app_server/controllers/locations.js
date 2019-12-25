a06c3857ccc421845798a020abd42e0cc479571f
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-25 17:37:11 +0300

    #9@1h12m: create layout of review page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,5 +5,5 @@ module.exports.locationInfo = function(req, res) {
     res.render('location-info', { title: 'Location info' });
 };
 module.exports.addReview = function(req, res) {
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,9 +6,9 @@ var ctrlOthers = require('../controllers/others');
 // Locations pages
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlOthers.about);
+router.get('/location/review/new', ctrlLocations.addReview);
 
 // Other pages
-router.get('/about', ctrlLocations.homelist);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
