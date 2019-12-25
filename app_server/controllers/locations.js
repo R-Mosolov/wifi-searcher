c4b224891d0f8115ca4820840a25e056e92b558e
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-25 05:21:03 +0300

    #7@3h02m: create layout of main page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,9 @@
-module.exports.homelist = function(req, res, next) {
-    res.render('index', { title: 'Home' });
+module.exports.homelist = function(req, res) {
+    res.render('locations-list', { title: 'Home' });
 };
-module.exports.locationInfo = function(req, res, next) {
+module.exports.locationInfo = function(req, res) {
     res.render('index', { title: 'Location info' });
 };
-module.exports.addReview = function(req, res, next) {
+module.exports.addReview = function(req, res) {
     res.render('index', { title: 'Add review' });
 };
