65b24a642d9381308cd4cf57893a0c737dcf3bdf
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-25 16:28:44 +0300

    #8@2h20m: create layout of details page about place

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,7 @@ module.exports.homelist = function(req, res) {
     res.render('locations-list', { title: 'Home' });
 };
 module.exports.locationInfo = function(req, res) {
-    res.render('index', { title: 'Location info' });
+    res.render('location-info', { title: 'Location info' });
 };
 module.exports.addReview = function(req, res) {
     res.render('index', { title: 'Add review' });
