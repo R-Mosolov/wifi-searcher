02532edf6190a9f56182ff56c56e4b089e9f3c2a
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-26 23:32:54 +0300

    #13@1h19m: move header and description of site to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,39 @@
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', { title: 'Поисковик Wi-Fi' });
+    res.render('locations-list', {
+        title: 'Поисковик Wi-Fi',
+
+        pageHeader: {
+            title: 'Поисковик Wi-Fi',
+            strapline: 'Найдите свободное место с Wi-Fi!'
+        },
+
+        locations: [{
+            name: 'Starbucks',
+            address: 'Казань, ул. Петербургская, д. 1',
+            facilities: ['Горячие напитки', 'Сладости Быстрый', 'Wi-Fi'],
+            distance: '130'
+        }, {
+            name: 'Coffee Like',
+            address: 'Казань, ул. Пушкина, д. 31',
+            facilities: ['Горячие напитки', 'Сладости Быстрый', 'Wi-Fi'],
+            distance: '100'
+        }, {
+            name: 'Кафетериус',
+            address: 'Казань, ул. Петербургская, д. 9',
+            facilities: ['Горячие напитки', 'Сладости Быстрый', 'Wi-Fi'],
+            distance: '150'
+        }]
+    });
 };
+
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'О месте | Поисковик Wi-Fi' });
+    res.render('location-info', {
+        title: 'О месте | Поисковик Wi-Fi'
+    });
 };
+
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', { title: 'Добавить отзыв | Поисковик Wi-Fi' });
+    res.render('location-review-form', {
+        title: 'Добавить отзыв | Поисковик Wi-Fi'
+    });
 };
