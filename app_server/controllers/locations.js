d31746ddf97b002fe52f451495d07897bd1dc9bf
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-27 10:13:34 +0300

    #14@1h25m: add places rating by using Bootstrap icons, create mixin

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,21 +7,27 @@ module.exports.homelist = function(req, res) {
             strapline: 'Найдите свободное место с Wi-Fi!'
         },
 
+        sidebar: 'Наше веб-приложение, "Поисковик Wi-Fi", поможет Вам найти подходящие, свободные места для отдыха ' +
+            'или работы.',
+
         locations: [{
+            name: 'Кафетериус',
+            address: 'Казань, ул. Петербургская, д. 9',
+            facilities: ['Горячие напитки', 'Удобные пуфики', 'Быстрый Wi-Fi'],
+            rating: 5,
+            distance: 150
+        }, {
             name: 'Starbucks',
             address: 'Казань, ул. Петербургская, д. 1',
-            facilities: ['Горячие напитки', 'Сладости Быстрый', 'Wi-Fi'],
-            distance: '130'
+            facilities: ['Горячие напитки', 'Сладости', 'Быстрый Wi-Fi'],
+            rating: 4,
+            distance: 130
         }, {
             name: 'Coffee Like',
             address: 'Казань, ул. Пушкина, д. 31',
-            facilities: ['Горячие напитки', 'Сладости Быстрый', 'Wi-Fi'],
-            distance: '100'
-        }, {
-            name: 'Кафетериус',
-            address: 'Казань, ул. Петербургская, д. 9',
-            facilities: ['Горячие напитки', 'Сладости Быстрый', 'Wi-Fi'],
-            distance: '150'
+            facilities: ['Самообслуживание', 'Сладости', 'Быстрый Wi-Fi'],
+            rating: 3,
+            distance: 100
         }]
     });
 };
