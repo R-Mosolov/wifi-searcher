20ef710d17d97c43de0c7f7cf69c126c0ece2ad9
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-29 11:25:06 +0300

    #19-20@1h33m: add model with events for MongoDB connections

---
+++ app_server/models/db.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost:27017/wifi-searcher';
+
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+// DISCONNECTION EVENTS
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    })
+});
+process.on('SIGTERM', function () {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    })
+});
+
+// DISCONNECTION MESSAGE GENERATOR
+var gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through: ' + msg);
+        callback();
+    })
+};
