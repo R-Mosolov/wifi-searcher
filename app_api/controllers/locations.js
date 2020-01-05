c4c2fc716d2a860004cf60082b69d7a0ffb566ba
R-Mosolov <r.v.mosolov@ya.ru>
2020-01-05 03:32:35 +0300

    #30-31@5h30m: add 2 methods of connection to MongoDB from API controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -7,10 +7,26 @@ var sendResponse = function(res, status, content) {
 };
 
 module.exports.locationsCreate = function (req, res) {
-    sendResponse(res, 200, {'status': 'success'});
+    sendResponse(res, 200, {
+        'status': 'success'
+    });
 };
 
-module.exports.locationsListByDistance = function (req, res) {};
-module.exports.locationsReadOne = function (req, res) {};
+module.exports.locationsReadOne = function(req, res) {
+    Location
+        .findById(req.params.locationid)
+        .exec(function(err, location) {
+            sendResponse(res, 200, location);
+        });
+};
+
+module.exports.locationsListByDistance = function (req, res) {
+    Location
+        .find()
+        .exec(function(err, locations) {
+            sendResponse(res, 200, locations);
+        });
+};
+
 module.exports.locationsUpdateOne = function (req, res) {};
 module.exports.locationsDeleteOne = function (req, res) {};
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,13 +1,12 @@
-var mongoose = require('mongoose');
-var Location = mongoose.model('Location');
-
 var sendResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
 
 module.exports.reviewsCreate = function (req, res) {
-    sendResponse(res, 200, {'status': 'success'});
+    sendResponse(res, 200, {
+        'status': 'success'
+    });
 };
 
 module.exports.locationsListByDistance = function (req, res) {};
