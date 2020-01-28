4e026e940ba555410640255c23a68e5d2730fe91
R-Mosolov <r.v.mosolov@ya.ru>
2020-01-28 22:24:09 +0300

    #85@0h24m: return Angular code to last working condition

--- public/angular/wifiSearcherApp.js
+++ public/angular/wifiSearcherApp.js
@@ -1,8 +1,7 @@
 angular
     .module('wifiSearcherApp', [])
     .controller('locationListCtrl', locationListCtrl)
-    .directive('ratingStars', ratingStars)
-    .service('wifiSearcherData', wifiSearcherData);
+    .directive('ratingStars', ratingStars);
 
 function ratingStars() {
     return {
@@ -13,30 +12,63 @@ function ratingStars() {
     };
 }
 
-function locationListCtrl($scope, wifiSearcherData) {
-    wifiSearcherData
-        .then(function(res) {
-            console.log(res.json());
-            $scope.locations = res;
-            console.log($scope.locations);
-            if (res.status === 200) {
-                console.log('Status 200: OK');
-                $scope.locations = res.body.json();
-                console.log($scope.locations);
-            }
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
+function locationListCtrl($scope) {
     $scope.reverse = true;
     $scope.propertyName = 'rating';
     $scope.sortBy = function(propertyName) {
         $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
         $scope.propertyName = propertyName;
     };
-}
-
-function wifiSearcherData() {
-    const responseLink = window.location + 'api/locations';
-    return fetch(responseLink);
+    $scope.locations = [{
+        "_id": "5e1a2b1865eb210a55b93f62",
+        "facilities": [
+            "Свежий кофе",
+            "Стильный интерьер",
+            "Бесплатная парковка",
+            "Красивый вид из окна"
+        ],
+        "rating": 4,
+        "name": "Starbucks",
+        "path": "id2359321",
+        "coordinates": {
+            "lat": "55.787992",
+            "lng": "49.1195083"
+        },
+        "address": "Казань, ул. Пушкина, д. 9",
+        "distance": 150
+    }, {
+        "_id": "5e1a34941c9d4400008f433d",
+        "facilities": [
+            "Быстрый Wi-Fi",
+            "Стильный интерьер",
+            "Красивый вид из окна",
+            "Вежливый персонал"
+        ],
+        "rating": 3,
+        "name": "Coffee Like",
+        "path": "id4292834",
+        "coordinates": {
+            "lat": "55.793775",
+            "lng": "49.124509"
+        },
+        "address": "Казань, ул. Пушкина, д. 31",
+        "distance": 130
+    }, {
+        "_id": "5e1a3bbd1c9d4400008f433e",
+        "facilities": [
+            "Свежий кофе",
+            "Быстрый Wi-Fi",
+            "Бесплатная парковка",
+            "Вежливый персонал"
+        ],
+        "name": "Кафетериус",
+        "path": "id1334242",
+        "coordinates": {
+            "lat": "55.785303",
+            "lng": "49.126433"
+        },
+        "rating": 5,
+        "address": "Казань, ул. Петербургская, д. 9",
+        "distance": 140
+    }];
 }
